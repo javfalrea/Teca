d96b54ec93deb62c9cc6a291e44caeb7ff12d59f
javfalrea <javierfalconreal2003@gmail.com>
2025-05-23 18:17:43 +0200

    Primer commit

---
+++ teca/src/main/java/com/teca/TecaApplication.java
@@ -0,0 +1,13 @@
+package com.teca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecaApplication.class, args);
+	}
+
+}
---
+++ teca/src/test/java/com/teca/TecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
